ee9c27b4973d25479f57f54c9ac57663cb1405b0
Cristian Ribeiro <cristian.rr99@gmail.com>
2025-07-09 14:03:54 -0300

    making register screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:rumo/features/onboarding/onboarding_screen.dart';
+import 'package:rumo/features/onboarding/routes/onboarding_routes.dart';
+import 'package:rumo/routes/app_router.dart';
+import 'package:rumo/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,76 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Rumo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //5
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.light,
-        ),
-        bottomSheetTheme: BottomSheetThemeData(
-          backgroundColor: Color(0xFFFFFFFF),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16),
-              topRight: Radius.circular(16),
-            ),
-            side: BorderSide(
-              color: Color(0xFFD9D9D9),
-              width: 1,
-            )
-          ),
-        ),
-      ),
-      home: OnboardingScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Rumo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('Counter'), Text('$counter times')],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
+      theme: AppTheme().theme,
+      routes: AppRouter.routes,
+      initialRoute: OnboardingRoutes.onboardingScreen,
     );
   }
 }
